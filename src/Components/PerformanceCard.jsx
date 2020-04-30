9d82bfb457a61eebbeb9a5b1684a154050ecb8d0
Andrew Coleman <andrewdavidcoleman@gmail.com>
2020-04-30 17:36:37 -0400

    Converted PerformanceCard to function component with hooks.

--- src/Components/PerformanceCard.jsx
+++ src/Components/PerformanceCard.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ResponsiveLine } from '@nivo/line'
 
 const data = [
     {
-        "id": "japan",
+        "id": "Erin",
         "color": "hsl(90, 70%, 50%)",
         "data": [
             {
-                "x": "plane",
+                "x": "date 1",
                 "y": 189
             },
             {
-                "x": "helicopter",
+                "x": "date 2",
                 "y": 264
             },
             {
-                "x": "boat",
+                "x": "date 3",
                 "y": 256
             }
         ]
     },
     {
-        "id": "france",
+        "id": "Leanne",
         "color": "hsl(174, 70%, 50%)",
         "data": [
             {
-                "x": "plane",
+                "x": "date 1",
                 "y": 222
             },
             {
-                "x": "helicopter",
+                "x": "date 2",
                 "y": 207
             },
         ]
     },
     {
-        "id": "us",
+        "id": "Andrew",
         "color": "hsl(197, 70%, 50%)",
         "data": [
             {
@@ -54,51 +54,73 @@ const data = [
     },
 ]
 
-class PerformanceCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+export default function PerformanceCard(props) {
 
-        };
+    const [athlete, setAthlete] = useState(props.athlete)
+    const [metcon, setMetcon] = useState(props.metcon)
+
+    //mock db data
+    const performances = [
+        {
+            "x": "plane",
+            "y": 27
+        },
+        {
+            "x": "helicopter",
+            "y": 289
+        },
+        {
+            "x": "boat",
+            "y": 183
+        }
+    ];
+
+    const data = [
+        {
+            "id": athlete.athleteId,
+            "color": 'hsl(197, 70%, 50%)',
+            "data": performances
+        }
+    ]
+
+    function handleClick() {
+        console.log(athlete);
+        console.log(metcon);
     }
 
-    render() {
-        return (
-            <div className="performance-card card full-border shad shad-hover cursor-pointer m-2">
-                <ResponsiveLine
-                    data={data}
-                    margin={{ top: 10, right: 30, bottom: 30, left: 50 }}
-                    xScale={{ type: 'point' }}
-                    yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-                    axisBottom={{
-                        orient: 'bottom',
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legendOffset: 36,
-                        legendPosition: 'middle'
-                    }}
-                    axisLeft={{
-                        orient: 'left',
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'count',
-                        legendOffset: -40,
-                        legendPosition: 'middle'
-                    }}
-                    colors={{ scheme: 'nivo' }}
-                    pointSize={10}
-                    pointColor={{ theme: 'background' }}
-                    pointBorderWidth={2}
-                    pointBorderColor={{ from: 'serieColor' }}
-                    pointLabel="y"
-                    pointLabelYOffset={-12}
-                    useMesh={true}
-                />
-            </div>
-        );
-    }
+    return (
+        <div className="performance-card card full-border shad shad-hover cursor-pointer m-2" onClick={handleClick}>
+            <ResponsiveLine
+                data={data}
+                margin={{ top: 10, right: 30, bottom: 30, left: 50 }}
+                xScale={{ type: 'point' }}
+                yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+                axisBottom={{
+                    orient: 'bottom',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legendOffset: 36,
+                    legendPosition: 'middle'
+                }}
+                axisLeft={{
+                    orient: 'left',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: 'count',
+                    legendOffset: -40,
+                    legendPosition: 'middle'
+                }}
+                colors={{ scheme: 'nivo' }}
+                pointSize={10}
+                pointColor={{ theme: 'background' }}
+                pointBorderWidth={2}
+                pointBorderColor={{ from: 'serieColor' }}
+                pointLabel="y"
+                pointLabelYOffset={-12}
+                useMesh={true}
+            />
+        </div>
+    );
 }
-
-export default PerformanceCard;
